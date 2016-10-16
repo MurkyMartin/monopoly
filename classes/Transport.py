aa1956b5d8aa428728893c4d2d58563c0f736a74
MurkyMartin <martinjkelly5@gmail.com>
2016-10-16 18:42:41 +0100

    Update Transport.py

--- classes/Transport.py
+++ classes/Transport.py
@@ -15,6 +15,13 @@ class Transport(Card):
 		#this is a second alternative
 		#False in position 0 corresponds to first transport son the board and so on
 		#True if transport owned
+		
+		##
+		#Could do the checking in the board
+		#If the board is a DDL, maintain a list of indexes of where the transports are
+		#then check those indexes
+		#could make this easier by having a 'owned_by' instance vairable that is a player object and check if a given
+		#player owns tranport(s)
 	
 	"""def other_trans_bought(self):
 		#if we are using a dictionary
