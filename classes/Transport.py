4b11148279a0bc9a4b6942f324cd33d743966580
gabystephanov <115342151@umail.ucc.ie>
2016-10-16 15:06:03 +0100

    Update Transport.py
    Suggestions for the player object on which transport card(s) they own

--- classes/Transport.py
+++ classes/Transport.py
@@ -4,3 +4,9 @@ class Transport(Card):
 
 	def __init__(self, name, cost=200):
 		Card.__init__(name,cost)
+		#perhaps this could declare the ownage of the transport cards and which ones???
+		#maybe have a dictionary which contain transport names and value defaults to False so
+		#more transports can be added and is more maintainable ??? {'Bus':False, 'Airport':False ...}
+		self.other_trans_owned = [False]*4 
+		#False in position 0 corresponds to first transport son the board and so on
+		#True if transport owned
